a0a5640b073711e7243eef5f4147b245ec178d3c
Ismael Ali <ismaelali78@outlook.com>
2022-08-16 15:12:01 +0100

    deploying to heroku

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from controllers import cars
+from waitress import serve
+from werkzeug import exceptions
 
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def welcome():
     return "Welcome to Cars API!"
+
 
 @app.route('/cars', methods=['GET', 'POST'])
 def car_list():
@@ -19,8 +23,9 @@ def car_list():
     resp, code = fns[request.method](request)
     return jsonify(resp), code
 
+
 @app.route('/cars/<int:car_id>', methods=['GET', 'DELETE', 'PUT'])
-def cat_handler(car_id):
+def car_handler(car_id):
     fns = {
         'GET': cars.show,
         'DELETE': cars.destroy,
@@ -30,5 +35,13 @@ def cat_handler(car_id):
     return jsonify(resp), code
 
 
+@app.errorhandler(exceptions.NotFound)
+def error_404(err):
+    return {'message': f'Page Not Found {err}'}, 404
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    serve(app, host='0.0.0.0', port=8080, url_scheme='https')
+
+    # Waitress to deploy on heroku!
